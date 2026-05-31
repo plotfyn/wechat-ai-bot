e3bc5b29bf5aa79c044bb5d58eeceef1ed5b8503
plotfyn <getfun2023@foxmail.com>
2026-06-01 04:43:19 +0800

    Add start.bat

---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "C:\Users\albus\Documents\Codex\2026-05-31\new-chat-2\outputs\wechat-bot-v2"
+"C:\Users\albus\AppData\Local\Programs\Python\Python312\python.exe" -u wechat_bot.py
+pause
